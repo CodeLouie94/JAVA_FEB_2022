ca3b23802f17b116fa461b7a30d45846749fc412
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2022-02-08 07:35:56 -0700

    prepare W1D2 OOP lecture

---
+++ W1D2_OOP/ShoppingCart.java
@@ -0,0 +1,9 @@
+this.items.add(item)
+public void add_item(String item, double price){
+public ArrayList<String> items = new ArrayList<String>();
+public String store;
+public double total;
+public class ShoppingCart {
+this.total += price;
+}
+}
