28ad5e8e8bccc17ca6828a5d2be13cac752af3a0
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2022-02-15 13:09:30 -0700

    add w2d2 api model - apiController

---
+++ W2D2_APIDemo/src/main/java/com/john/W2D2_APIDemo/W2D2ApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.john.W2D2_APIDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2D2ApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W2D2ApiDemoApplication.class, args);
+	}
+
+}
---
+++ W2D2_APIDemo/src/test/java/com/john/W2D2_APIDemo/W2D2ApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.W2D2_APIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2D2ApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
